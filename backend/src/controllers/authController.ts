57ed583eab995d2c4659baf953b1a6b5f37a2922
dhakshinesh <dhakshineshanandh@gmail.com>
2025-06-06 16:30:51 +0530

    Fixed Error

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -11,13 +11,13 @@ export const loginUser = async (req: Request, res: Response) => {
     const userData = req.body;
     const user_str = JSON.stringify(userData, null, 2)
     res.json({ message: "User created", user_str })
-    // const {name,email,password} = req.body;
-    // //Save data in MongoDB
-    // try{
-    //     const newUser = new User({ name , email, password });
-    //     await newUser.save();
-    // }catch{
-    //     console.error("Data not Saved(Error)")
-    // }
+    const {name,email,password} = req.body;
+    //Save data in MongoDB
+    try{
+        const newUser = new User({ name , email, password });
+        await newUser.save();
+    }catch{
+        console.error("Data not Saved(Error)")
+    }
 };
 
