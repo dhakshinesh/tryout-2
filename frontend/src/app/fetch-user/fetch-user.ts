577aac5c1a7e1563f121082821596aebec59d6a5
dhakshinesh <dhakshineshanandh@gmail.com>
2025-06-06 20:51:40 +0530

    Added Functionalities :

--- frontend/src/app/fetch-user/fetch-user.ts
+++ frontend/src/app/fetch-user/fetch-user.ts
@@ -1,20 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Data } from '../service/data';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-fetch-user',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './fetch-user.html',
   styleUrl: './fetch-user.css'
 })
 export class FetchUser implements OnInit {
     users: any[] = [];
+    editingUser: any = null;
+    editedData = { name: '', email: '', password: '' };
+
     constructor(private dataService: Data) {}
     ngOnInit() {
+        this.dataService.getUsers().subscribe(users => {
+            this.fetchUsers()
+        });
+    }
+    editUser(user : any){
+        // Logic to edit a user
+        this.editingUser = user;
+        this.editedData = { ...user };
+    }
+
+    cancelEdit() {
+        this.editingUser = null;
+    }
+    fetchUsers() {
         this.dataService.getUsers().subscribe(users => {
             this.users = users;
             console.log('Fetched users:', users);
         });
+    }
+    saveEdit() {
+        this.dataService.updateUser(this.editingUser._id, this.editedData).subscribe(() =>{
+            this.fetchUsers();
+        });
+    }
+
+    deleteUser(userId: string){
+        this.dataService.deleteUser(userId).subscribe(() => {
+            this.fetchUsers();
+        });
+    
     }
 }
--- frontend/src/app/service/data.ts
+++ frontend/src/app/service/data.ts
@@ -25,4 +25,12 @@ export class Data {
     getUsers(): Observable<any[]> {
         return this.http.get<any[]>('http://localhost:5000/api/get');
     }
+    
+    updateUser(id: string, data: any): Observable<any> {
+        return this.http.put(`http://localhost:5000/api/update/${id}`, data);
+    }
+
+    deleteUser(id: string): Observable<any> {
+        return this.http.delete(`http://localhost:5000/api/delete/${id}`);
+    }
 }
