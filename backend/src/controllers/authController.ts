382fa16f13b16448863a57502c613b2038e183d8
Kanishkluffy <ktadhithya2006@gmail.com>
2025-06-06 16:07:04 +0530

    Controller service

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,10 +1,23 @@
-import { NextFunction, Request, Response } from "express";
+import {  Request, Response } from "express";
 import User from '../models/Item';
 
+interface User{
+    name:String,
+    email:String,
+    password:String
+}
 
-export const loginUser = (req: Request, res: Response) => {
+export const loginUser = async (req: Request, res: Response) => {
     const userData = req.body;
     const user_str = JSON.stringify(userData, null, 2)
     res.json({ message: "User created", user_str })
+    // const {name,email,password} = req.body;
+    // //Save data in MongoDB
+    // try{
+    //     const newUser = new User({ name , email, password });
+    //     await newUser.save();
+    // }catch{
+    //     console.error("Data not Saved(Error)")
+    // }
 };
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,31 +3,23 @@ import mongoose from "mongoose";
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import router from "./routers/routes";
+import dotenv from "dotenv"
 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+dotenv.config();
 
 //middlewares
 app.use(cors());
 app.use(bodyParser.json());
 
 //connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/user-form-db')
-    .then(() => {
-        console.log('Connected to MongoDB');
-        // Start server only after DB connects
-        app.listen(PORT, () => {
-            console.log(`Server running on http://localhost:${PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.error('MongoDB connection error:', err);
-    });
+const mongo_uri = process.env.MONGO_URI!;
+mongoose.connect(mongo_uri)
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("Failed to Connect",err));
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
 
 app.post("/api/register", router)
 
